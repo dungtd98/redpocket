f8975bd418cd13e83da6e49ae6dc6b46e3c34ff3
dungtd <dungtd@relipasoft.com>
2024-08-26 06:47:14 +0700

    UPdate

--- wallet/urls.py
+++ wallet/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import CreateGiveawayPouchView, ClaimPouchTokenView, GetPouchListInfoView, CreateStakeView, GetActiveStakeView
 
 urlpatterns = [
-    path('create-giveaway-pouch', CreateGiveawayPouchView.as_view(), name='create-giveaway-pouch'),
-    path('claim-pouch-token', ClaimPouchTokenView.as_view(), name='claim-pouch-token'),
-    path('get-pouch-list-info', GetPouchListInfoView.as_view(), name='get-pouch-list-info'),
+    path('coin-pouch/create', CreateGiveawayPouchView.as_view(), name='create-giveaway-pouch'),
+    path('coin-pouch/claim', ClaimPouchTokenView.as_view(), name='claim-pouch-token'),
+    path('coin-pouch', GetPouchListInfoView.as_view(), name='get-pouch-list-info'),
     path('create-stake', CreateStakeView.as_view(), name='create-stake'),
     path('get-active-stake', GetActiveStakeView.as_view(), name='get-active-stake'),
     
